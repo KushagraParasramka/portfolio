d438b8db5456245c4f69af54925c0bad8232746d
Kushagra Parasramka <65237398+KushagraParasramka@users.noreply.github.com>
2024-08-25 16:23:14 +0530

    added a little responsiveness

--- src/projectdisplay/Displaybody.jsx
+++ src/projectdisplay/Displaybody.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import Back from '../utils/Back.jsx';
 
 function Displaybody() {
     const location = useLocation();
@@ -25,13 +26,43 @@ function Displaybody() {
     }
 
     if (!data) {
-        return <div>Loading...</div>;
+        return <div>NOT UPDATED YET</div>;
     }
 
     return (
-        <div>
-            <h1>Displaybody</h1>
-            <div>{data.projectName}</div>
+        <div className='body-container'>
+            <Back />
+            <div className='image-box'>
+                <img src={`../src/assets/${data.projectImage}`} style={{ width: "100%", height: "126px", objectFit: "cover" }} alt="image..." />
+            </div>
+            <div className="headline">{data.projectName}</div>
+            <div className='tehstack'>
+                <span className='heading-techstack'>techstack :</span>
+                {data.techstack}
+            </div>
+            <div className='code-links'>
+            <a href={data.codeLink} target="_blank" rel="noopener noreferrer">
+                code
+            </a>
+            <a href={data.liveLink} target="_blank" rel="noopener noreferrer">
+                live
+            </a>
+            <div className='project-content'>
+                {data.body.map((elem) => {
+                    if(elem.startsWith("https")) {
+                        return (
+                            <div className='image-box'>
+                                <img src={elem} style={{ width: "100%", height: "auto", objectFit: "cover" }} alt="image..." />
+                            </div>
+                        )
+                    }
+                    return (
+                        <div>{elem}</div>
+                    )
+                })}
+            </div>
+            </div>
+
         </div>
     );
 }
--- src/utils/Projectbox.jsx
+++ src/utils/Projectbox.jsx
@@ -5,7 +5,7 @@ function Projectbox({projectId, projectName, projectImage}) {
   return (
     <div className='project-box'>
         <div className='image-box'>
-        <img src={`./src/assets/${projectImage}`} style={{ width: "555px", height: "126px", objectFit: "cover" }} alt="image..." />
+        <img src={`./src/assets/${projectImage}`} style={{ width: "100%", height: "126px", objectFit: "cover" }} alt="image..." />
         </div>
         <div className='detail-box'>{projectName}</div>
         <Link className='read-more' to={`/project/${projectId}`} state={{ projectId }}>read more</Link>
